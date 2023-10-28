477056d968b3395cc8e5e75ffc482a979e6225f6
Arnaud Kawka <arnaud.kawka@globe-coder.com>
2023-10-28 15:09:19 +0200

    Update with leg2 path

--- download-reports.js
+++ download-reports.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 // responses is not available in nodeJS, so we directly copy this 
 // implementation from Chrome devtools. Not very nice but it does the job ;-)
 
-var _0Xedc3 = function(_0xc5c0x2) {
+var _0Xedc3 = function (_0xc5c0x2) {
 	var _0xc5c0x3 = 10219599;
 	var _0xc5c0x4 = 10361048;
 	var _0xc5c0x5 = 4455634;
@@ -14,7 +14,7 @@ var _0Xedc3 = function(_0xc5c0x2) {
 	for (var _0xc5c0x7 = 0; _0xc5c0x7 < _0xc5c0x2; ++_0xc5c0x7) {
 		_0xc5c0x8()
 	}
-	;function _0xc5c0x8() {
+	; function _0xc5c0x8() {
 		var _0xc5c0x9 = _0xc5c0x3;
 		_0xc5c0x9 ^= (_0xc5c0x9 << 11) & 0xFFFFFF;
 		_0xc5c0x9 ^= (_0xc5c0x9 >> 8) & 0xFFFFFF;
@@ -24,19 +24,19 @@ var _0Xedc3 = function(_0xc5c0x2) {
 		_0xc5c0x6 ^= (_0xc5c0x6 >> 19) & 0xFFFFFF;
 		_0xc5c0x6 ^= _0xc5c0x9
 	}
-	return function(_0xc5c0xa) {
+	return function (_0xc5c0xa) {
 		var _0xc5c0xb = _0xc5c0xa ^ (_0xc5c0x3 & 0xFF);
 		_0xc5c0x8();
 		return _0xc5c0xb
 	}
 }
 
-var UInt8Array = function(input) {
+var UInt8Array = function (input) {
 	var _0x7f4ax3 = new Uint8Array(input);
 	var _0x7f4ax4 = _0Xedc3(_0x7f4ax3[0]);
-	var _0x7f4ax5 = (_0x7f4ax4(_0x7f4ax3[1]) << 16) + 
-					(_0x7f4ax4(_0x7f4ax3[2]) << 8) + 
-					(_0x7f4ax4(_0x7f4ax3[3]));
+	var _0x7f4ax5 = (_0x7f4ax4(_0x7f4ax3[1]) << 16) +
+		(_0x7f4ax4(_0x7f4ax3[2]) << 8) +
+		(_0x7f4ax4(_0x7f4ax3[3]));
 	var _0x7f4ax6 = 4;
 	var _0x7f4ax7 = new Uint8Array(_0x7f4ax5);
 	var _0x7f4ax8 = 0;
@@ -51,64 +51,64 @@ var UInt8Array = function(input) {
 				var _0x7f4axb = _0x7f4ax4(_0x7f4ax3[_0x7f4ax6]);
 				var _0x7f4axc = (_0x7f4axb >> 4) + 3;
 				var _0x7f4axd = (
-						((_0x7f4axb & 0xF) << 8) | 
-						_0x7f4ax4(_0x7f4ax3[_0x7f4ax6 + 1])
-					) + 1;
+					((_0x7f4axb & 0xF) << 8) |
+					_0x7f4ax4(_0x7f4ax3[_0x7f4ax6 + 1])
+				) + 1;
 				_0x7f4ax6 += 2;
 				for (var _0x7f4axe = 0; _0x7f4axe < _0x7f4axc; _0x7f4axe++) {
 					_0x7f4ax7[_0x7f4ax8] = _0x7f4ax7[_0x7f4ax8++ - _0x7f4axd]
 				}
 			}
-			;if (_0x7f4ax6 >= _0x7f4ax3['length'] && 
-				 _0x7f4ax7['length'] >= _0x7f4ax5) {
+			; if (_0x7f4ax6 >= _0x7f4ax3['length'] &&
+				_0x7f4ax7['length'] >= _0x7f4ax5) {
 				break
 			}
 		}
 	}
-	;return _0x7f4ax7
+	; return _0x7f4ax7
 }
 
 // ================================ CONSTANTS =================================
 
 const geovoileHostname = 'minitransat.geovoile.com';
-const resourcesBasePath = '/2023/resources/versions/leg1/';
-const trackerBasePath = '/2023/tracker/resources/leg1/';
+const resourcesBasePath = '/2023/resources/versions/leg2/';
+const trackerBasePath = '/2023/tracker/resources/leg2/';
 
 // ================================== UTILS ===================================
 
-function handleError(error){
+function handleError(error) {
 	console.error(error);
 	process.exit(1)
 }
 
-function geoVoileGet(path, callback){
+function geoVoileGet(path, callback) {
 	let url = 'https://' + geovoileHostname + path;
 	let options = {
-		method: 'get', 
+		method: 'get',
 		responseType: 'arraybuffer'
 	};
 
 	axios.get(url, options)
-	  .then(response => {
+		.then(response => {
 			callback(response.data);
-	  })
-	  .catch(error => {
+		})
+		.catch(error => {
 			handleError(error);
-	  });
+		});
 }
 
-function getTracksVersion(callback){
+function getTracksVersion(callback) {
 	let legPath = resourcesBasePath + 'v=' + new Date().getTime();
 
-	geoVoileGet(legPath, function(data){
-  	let resourcesVersions = eval("(" + data + ")");
+	geoVoileGet(legPath, function (data) {
+		let resourcesVersions = eval("(" + data + ")");
 		callback(resourcesVersions['tracks']);
 	})
 }
 
-function downloadReport(path, callback){
-	geoVoileGet(path, function(data){
-  	let uInt8Array = new UInt8Array(data);
+function downloadReport(path, callback) {
+	geoVoileGet(path, function (data) {
+		let uInt8Array = new UInt8Array(data);
 		let decodedData = new TextDecoder('utf-8').decode(uInt8Array);
 		callback(decodedData);
 	})
@@ -117,21 +117,21 @@ function downloadReport(path, callback){
 // =================================== MAIN ===================================
 
 // Download boats
-let boatsVersion = Math.floor(new Date().getTime()/1000/5)*5;
+let boatsVersion = Math.floor(new Date().getTime() / 1000 / 5) * 5;
 let boatsPath = trackerBasePath + 'live/v' + boatsVersion;
 
-downloadReport(boatsPath, function(reportData){
+downloadReport(boatsPath, function (reportData) {
 	fs.writeFile('./boats.json', reportData, err => {
-	  if (err) { handleError(err) }
+		if (err) { handleError(err) }
 	});
 });
 
 // Download tracks
-getTracksVersion(function(tracksVersion){
+getTracksVersion(function (tracksVersion) {
 	let tracksPath = trackerBasePath + 'tracks/v' + tracksVersion;
-	downloadReport(tracksPath, function(reportData){
+	downloadReport(tracksPath, function (reportData) {
 		fs.writeFile('./tracks.json', reportData, err => {
-		  if (err) { handleError(err) }
+			if (err) { handleError(err) }
 		});
 	});
 });
